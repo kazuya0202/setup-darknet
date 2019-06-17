383e7344a5c854f79c88c4ca0cb2554ec8135351
frontier9\Student <kazuya7187@gmail.com>
2019-06-17 16:35:49 +0900

    update

--- prepare.sh
+++ prepare.sh
@@ -19,4 +19,7 @@ apt-fast install -y build-essential software-properties-common byobu git htop ma
 ###########################
 # ./scripts/install-nvidia-driver.sh
 
-apt-fast install -y nvidia-340 libnvidia-cfg1-390 libnvidia-common-390 libnvidia-computee-390 libnvidia-decode-390 libnvidia-encode-390 libnvidia-fbc1-390 libnvidia-gl-390 libnvidia-ifr1-390 nvidia-dkms-390 nvidia-driver390 nvidia-kernel-common-390 nvidia-kernel-source-390 nvidia-modprobe nvidia-opencl-dev nvidia-settings nvidia-utils-390 xserver-xorg-video-nvidia-390
+apt-fast install -y nvidia-340 libnvidia-cfg1-390 libnvidia-common-390 libnvidia-compute-390 libnvidia-decode-390 libnvidia-encode-390 libnvidia-fbc1-390 libnvidia-gl-390 libnvidia-ifr1-390 nvidia-dkms-390 nvidia-driver-390 nvidia-kernel-common-390 nvidia-kernel-source-390 nvidia-modprobe nvidia-opencl-dev nvidia-settings nvidia-utils-390 xserver-xorg-video-nvidia-390
+
+apt-fast autoremove
+apt autoremove
